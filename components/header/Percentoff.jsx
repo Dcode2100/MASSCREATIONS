8267d8d8fecdb0b48889d37f07f66b1a364a3ba4
Dikshant Solanki <dcode2100@gmail.com>
2023-04-07 00:17:58 +0530

    header ui

---
+++ components/header/Percentoff.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Percentoff = () => {
+  return (
+    <div>
+      <div className="bg-blue-200 h-9 flex items-center justify-center text-black text-center">
+        <p>Get 10% off on your first order</p>
+        <p>20% off on prepaid discount</p>
+      </div>
+    </div>
+  );
+};
+
+export default Percentoff;
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    
+    extend: {
+      
+    },
   },
   plugins: [],
 }

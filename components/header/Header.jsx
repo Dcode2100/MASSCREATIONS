23210e7ad6a782c679d90953526eba4fb0a1f537
Dikshant Solanki <dcode2100@gmail.com>
2023-04-06 23:02:00 +0530

    tailwind and tsx config

---
+++ components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="Header h-[5rem] bg-white flex justify-between items-center">
+        <div className=" text-black bg-blue-200 max-w-min">KEYPINI</div>
+        <div className="links text-blue-800">
+          <li href="/">Home</li>
+          <li href="/"></li>
+          <li href="/">Home</li>
+          <li href="/">Home</li>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;

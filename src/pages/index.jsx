f4dfd1b99fcead6b391ab6908d0951b0a8e02c52
Dikshant Solanki <dcode2100@gmail.com>
2023-04-27 22:44:22 +0530

    supabase config and initial setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../../lib/supabaseClient";
+import React from "react";
+
+function index() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      // Fetch all categories and their associated products
+      const { data: categoriesAndProducts, error } = await supabase
+        .from("products")
+        .select("*")
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      // Process the results
+      // const categoryMap = {};
+      // categoriesAndProducts.forEach((row) => {
+      //   // Create the category object if it doesn't exist
+      //   if (!categoryMap[row.category_id]) {
+      //     categoryMap[row.category_id] = {
+      //       id: row.category_id,
+      //       name: row.name,
+      //       products: [],
+      //     };
+      //   }
+
+      //   // Add the product to the category's products array
+      //   categoryMap[row.category_id].products.push({
+      //     id: row.id,
+      //     name: row.name,
+      //     description: row.description,
+      //     price: row.price,
+      //     image: row.image,
+      //   });
+      // });
+
+      // // Convert the category map to an array
+      // const categoryArray = Object.values(categoryMap);
+      setCategories(categoriesAndProducts);
+      console.log(categoriesAndProducts);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>My Ecommerce Store</h1>
+      <ul>
+        {categories.map((category) => ( 
+          <li key={category.id}>
+            <h2>{category.name}</h2>
+            
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}
+
+export default index;

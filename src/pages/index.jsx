0b5e7044bb5068cc3f63afa06b02f000c29ca67c
abhishek <purohitabhishek308@gmail.com>
2023-04-30 10:10:46 +0530

    wert

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { supabase } from "../../lib/supabaseClient";
 import Herosection from "../../components/home/Herosection";
 import Arrivals from "../../components/home/Arrivals"
+
 import React from "react";
 
 function index() {
